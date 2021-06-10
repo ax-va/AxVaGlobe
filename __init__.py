c566c0d5d01ad08ede82bb83f6fefe88a3024747
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-06-10 16:43:24 +0200

    init

--- __init__.py
+++ __init__.py
@@ -2,9 +2,8 @@
 
 
 class PackageInformation:
-    AUTHORS = ['Alexander Vasiliev']
-    CONTACTS = ['vasiliev.informatik@t-online.de']
-    # DEPENDENCIES = {'pytest': {'name': 'pytest', 'version': '6,2.4'}}
+    AUTHORS = {'Vasiliev': {'name': 'Alexander Vasiliev', 'email': 'vasiliev.informatik@t-online.de'}}
+    # DEPENDENCIES = {'pytest': {'name': 'pytest', 'version': '6.2.4'}}
     RELEASE_DATE = '2021-05-15'
     LIBRARY = {'pyglobe3d': {'name': 'pyglobe3d', 'version': '0.0.1'}}
     PYTHON_VERSION = '3.8'
