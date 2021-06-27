b22692145656b33df3feb20daa6660ee0e745973
Alexander Vasiliev <85578981+ka-tet-19@users.noreply.github.com>
2021-06-27 22:41:58 +0200

    Update matrix.py

--- opengl/matrices/matrix.py
+++ opengl/matrices/matrix.py
@@ -18,8 +18,8 @@ class MatrixGL:
         return self._matrix
 
     def set_identity(self):
-        for value in map(lambda i, j: 0. if i != j else 1., *itertools.product(range(4), range(4)))
-            self._matrix[i][j] = value
+        for i, j in itertools.product(range(4), range(4))
+            self._matrix[i][j] = 0. if i != j else 1.
 
 
 if __name__ == '__main__':
