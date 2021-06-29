36bbbbe1026b0872e8b838c0938c2e61cb1c2e56
Alexander Vasiliev <85578981+ka-tet-19@users.noreply.github.com>
2021-06-29 16:15:30 +0200

    Update model_view.py

--- opengl/matrices/model_view.py
+++ opengl/matrices/model_view.py
@@ -1,11 +1,11 @@
 import math
 
-from pyglobe3d.opengl.matrices.mat4x4 import Mat4X4
+from pyglobe3d.opengl.matrices.matrix4x4 import Matrix4X4
 
 
-class ModelView(Mat4X4):
+class ModelView(Matrix4X4):
     def __init__(self):
-        Mat4X4.__init__(self)
+        Matrix4X4.__init__(self)
         self._rotate_funcs = {
             'x': self._rotate_around_x,
             'y': self._rotate_around_y,
