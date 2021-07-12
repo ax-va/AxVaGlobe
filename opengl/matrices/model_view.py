895c52d2fe3dbda566c8ba4ccdb383a90359a8d8
Alexander Vasiliev <85578981+ka-tet-19@users.noreply.github.com>
2021-07-12 03:15:03 +0200

    Update model_view.py

--- opengl/matrices/model_view.py
+++ opengl/matrices/model_view.py
@@ -16,7 +16,7 @@ class ModelView(OpenGLMatrix):
         radians = math.radians(degrees)
         cos_t = math.cos(radians)
         sin_t = math.sin(radians)
-        if around == 'x' or around == 'y' or around == 'z':
+        if around in 'xyz':
             self._rotate_funcs[around](cos_t, sin_t)
         else:
             self._rotate_around_axis(around, cos_t, sin_t)
