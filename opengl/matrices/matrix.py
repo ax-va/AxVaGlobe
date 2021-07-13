8717044b3f05f74e45173a784a4a3f0053ab9d42
Alexander Vasiliev <85578981+ka-tet-19@users.noreply.github.com>
2021-07-14 00:02:38 +0200

    Update matrix.py

--- opengl/matrices/matrix.py
+++ opengl/matrices/matrix.py
@@ -45,9 +45,10 @@ class OpenGLMatrix:
      
     def set_entries(self, entries):
         """
-        instance.set_entries(entries) sets entries of instance._matrix given by
-        an iterable object containing iterable subobjects with a row index, a column 
-        index, and a value.  For example, entries = [[0, 0, .25], [0, 1, .5] set
+        instance.set_entries(entries) sets the entries of the matrix instance._matrix 
+        given by an iterable object containing iterable subobjects with a row index, 
+        a column index, and a value. 
+        For example, entries = [[0, 0, .25], [0, 1, .5] set
         instance._matrix[0][0] = .25 and instance._matrix[0][1] = .5.
         """
         for i, j, v in entries:
