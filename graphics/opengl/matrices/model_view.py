0e85a85b36f42a313d8a0ed54e789cd02d11ed1a
Alexander Vasiliev <85578981+ka-tet-19@users.noreply.github.com>
2021-07-16 12:29:21 +0200

    Update model_view.py

--- graphics/opengl/matrices/model_view.py
+++ graphics/opengl/matrices/model_view.py
@@ -49,7 +49,7 @@ class ModelView(OpenGLMatrix):
             [_1_minus_cos_t * xy + sin_t * z, cos_t + _1_minus_cos_t * y**2, _1_minus_cos_t * yz - sin_t * x],
             [_1_minus_cos_t * xz - sin_t * y, _1_minus_cos_t * yz + sin_t * x, cos_t + _1_minus_cos_t * z**2]
         ]
-        self.multiply(by_matrix=OpenGLMatrix(matrix=rotation_matrix), way='left')
+        self.multiply(by_matrix=OpenGLMatrix(matrix=rotation_matrix))
 
     def _rotate_around_x(self, cos_t, sin_t):
         """
