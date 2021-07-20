d10b14946a4736471c85dc82457812466171358b
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-07-20 17:08:10 +0200

    OpenGL perspective

--- graphics/opengl/matrices/perspective.py
+++ graphics/opengl/matrices/perspective.py
@@ -13,12 +13,12 @@ class Perspective(Projection):
                    [0., 0., -(far + near) / (far - near), -2. * far * near / (far - near)],
                    [0., 0., -1., 0.]]
 
-        :param right: distance of the right clipping plane from the origin
-        :param top: distance of the top clipping plane from the origin
+        :param right: right clipping plane from the origin
+        :param top: top clipping plane from the origin
         :param near: distance of the near clipping plane from the eye in the origin
         :param far: distance of the far clipping plane from the eye in the origin
-        :param left: distance of the left clipping plane from the origin
-        :param bottom: distance of the bottom clipping plane from the origin
+        :param left: left clipping plane from the origin
+        :param bottom: bottom clipping plane from the origin
         """
         Projection.__init__(self, left, right, bottom, top, near, far)
         self._set_matrix_entries()
