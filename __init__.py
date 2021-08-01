d5164637a7c8cc45dc412767ee72f4a900acfc88
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-08-01 21:39:18 +0200

    Tests of triangle nodes

--- __init__.py
+++ __init__.py
@@ -2,11 +2,11 @@
 
 
 class PackageInformation:
-    AUTHORS = {'Vasiliev': {'name': 'Alexander Vasiliev', 'email': 'vasiliev.informatik@t-online.de'}}
+    AUTHORS = ['Alexander Vasiliev']
     DEPENDENCIES = {
-        'pytest': {'name': 'pytest', 'version': '6.2.4'},
-        'numpy': {'name': 'numpy', 'version': '1.21.0'},
+        'pytest': {'version': '6.2.4'},
+        'numpy': {'version': '1.21.1'},
     }
-    LIBRARY = {'pyglobe3d': {'name': 'pyglobe3d', 'version': '0.0.1'}}
+    LIBRARY = {'pyglobe3d': {'version': '0.0.1'}}
     LIBRARY_RELEASE_DATE = '2021-05-15'
     PYTHON_VERSION = '3.8'
