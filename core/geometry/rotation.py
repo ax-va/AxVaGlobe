b4198e7050ae6ca9b25a17f8bf734a57233e1dc8
Alexander Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-08-07 10:04:35 +0200

    Update rotation.py

--- core/geometry/rotation.py
+++ core/geometry/rotation.py
@@ -14,9 +14,9 @@ def get_rotation_matrix(x, y, z, cos_t, sin_t) -> List:
             [_1_minus_cos_t * xz - sin_t * y, _1_minus_cos_t * yz + sin_t * x, cos_t + _1_minus_cos_t * z**2]]
 
 
-def rotate(vertex0: np.array, vertex1: np.array, radians: float) -> np.array:
-    vector = np.cross(vertex0, vertex1)
-    x, y, z = vector / np.sqrt(np.sum(vector**2))
+def get_rotated_vertex(vertex0: np.array, vertex1: np.array, radians: float) -> np.array:
+    normal = np.cross(vertex0, vertex1)
+    x, y, z = normal / np.sqrt(np.sum(normal**2))
     cos_t = math.cos(radians)
     sin_t = math.sin(radians)
     rotation_matrix = np.array(get_rotation_matrix(x, y, z, cos_t, sin_t))
