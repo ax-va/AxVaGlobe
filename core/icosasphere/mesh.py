0eb690c3e41da10f32aec4a5696f17e9115f6efe
Alexander Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-08-07 10:10:24 +0200

    Update mesh.py

--- core/icosasphere/mesh.py
+++ core/icosasphere/mesh.py
@@ -11,22 +11,24 @@ class Mesh:
         self._icosahedron = Icosahedron(radius=radius)
         self._cash_icosahedron_vertices()
 
-    def add_edge_nodes(self, edges, nodes):
+    def add_nodes(self, nodes):
+        for node in nodes:
+            if node.index not in self._vertex_cash:
+                self._add_edge_nodes(node.nearest_layer_edges, node.nearest_layer_edge_nodes)
+                if node.nearest_layer_edges == 2:  
+                # otherwise the node is on an edge and was already added into the cash
+                    vertex0 = self._vertex_cash[node.nearest_layer_edge_nodes.node0.index]
+                    vertex1 = self._vertex_cash[node.nearest_layer_edge_nodes.node1.index]
+                    radians = ...
+
+    def _add_edge_nodes(self, edges, nodes):
         for edge, node in zip(edges, nodes):
             if node.index not in self._vertex_cash:
                 vertex0 = self._vertex_cash[edge.icosahedron_nodes.node0.index]
                 vertex1 = self._vertex_cash[edge.icosahedron_nodes.node1.index]
                 radians = self._icosahedron.theta / self._logic_mesh.GRID.PARTITION * ...
                 self._vertex_cash[node.index] = get_rotated_vertex(vertex0, vertex1, radians)
-
-    def add_node(self, node):
-        if node.index not in self._vertex_cash:
-            self.add_edge_nodes(node.nearest_layer_edges, node.nearest_layer_edge_nodes)
-            if node.nearest_layer_edges == 2:  # otherwise the node is on an edge and was already added into the cash
-                vertex0 = self._vertex_cash[node.nearest_layer_edge_nodes.node0.index]
-                vertex1 = self._vertex_cash[node.nearest_layer_edge_nodes.node1.index]
-                radians = ...
-
+    
     def _cash_icosahedron_vertices(self):
         icosahedron_vertices = self._icosahedron.vertex_np_array
         icosahedron_nodes_indices = (node.index for node in self._logic_mesh.ICOSAHEDRON_NODES)
