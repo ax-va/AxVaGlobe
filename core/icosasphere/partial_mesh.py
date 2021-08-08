68f2f1ab88a6cb561a864f5b7f39ed8189103fa5
Alexander Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-08-09 00:07:16 +0200

    Update partial_mesh.py

--- core/icosasphere/partial_mesh.py
+++ core/icosasphere/partial_mesh.py
@@ -1,6 +1,6 @@
 import math
 
-from pyglobe3d.core.geometry.rotation import get_angle_between, get_rotated_vertex
+from pyglobe3d.core.geometry.rotation import get_angle_between, get_rotated_np_vertex
 from pyglobe3d.core.icosasphere.any_mesh import AnyMesh
 
 
@@ -12,36 +12,36 @@ class PartialMesh(AnyMesh):
         self._add_icosahedron_nodes()
 
     @property
-    def vertex_cash(self):
-        return self._vertex_cash
+    def vertex_dict(self):
+        return self._vertex_dict
 
     def add_nodes(self, nodes):
         for node in nodes:
-            if node.index not in self._vertex_cash:
+            if node.index not in self._vertex_dict:
                 self._add_node(node)
             for neighbor in node.neighboring_nodes:
-                if neighbor.index not in self._vertex_cash:
+                if neighbor.index not in self._vertex_dict:
                     self._add_node(neighbor)
             for triangle in node.adjacent_triangles:
-                if (triangle.index + self._index_offset) not in self._vertex_cash:
+                if (triangle.index + self._index_offset) not in self._vertex_dict:
                     ...
 
     def _add_edge_node(self, edge, node):
         node0, node1 = edge.icosahedron_nodes
-        vertex0 = self._vertex_cash[node0.index]
-        vertex1 = self._vertex_cash[node1.index]
+        np_vertex0 = self._vertex_dict[node0.index]
+        np_vertex1 = self._vertex_dict[node1.index]
         radians = self._theta_factor * node0.division_ratios.ratio0
-        self._vertex_cash[node.index] = get_rotated_vertex(vertex0, vertex1, radians)
+        self._vertex_dict[node.index] = get_rotated_np_vertex(np_vertex0, np_vertex1, radians)
 
     def _add_edge_nodes(self, edges, nodes):
         for edge, node in zip(edges, nodes):
-            if node.index not in self._vertex_cash:
+            if node.index not in self._vertex_dict:
                 self._add_edge_node(edge, node)
 
     def _add_icosahedron_nodes(self):
         icosahedron_vertices = self.icosahedron.vertex_np_array
         icosahedron_nodes_indices = (node.index for node in self.logic_mesh.ICOSAHEDRON_NODES)
-        self._vertex_cash = dict(zip(icosahedron_nodes_indices, icosahedron_vertices))
+        self._vertex_dict = dict(zip(icosahedron_nodes_indices, icosahedron_vertices))
 
     def _add_node(self, node):
         if node.nearest_layer_edges_number == 2:  # The node is not on an edge
@@ -52,11 +52,11 @@ class PartialMesh(AnyMesh):
 
     def _add_non_edge_node(self, node):
         node0, node1 = node.nearest_layer_edge_nodes
-        vertex0 = self._vertex_cash[node0.index]
-        vertex1 = self._vertex_cash[node1.index]
+        np_vertex0 = self._vertex_cash[node0.index]
+        np_vertex1 = self._vertex_cash[node1.index]
         ratio0, ratio1 = node.division_ratios
-        radians = get_angle_between(vertex0, vertex1) * (ratio0 / (ratio0 + ratio1))
-        self._vertex_cash[node.index] = get_rotated_vertex(vertex0, vertex1, radians)
+        radians = get_angle_between(np_vertex0, np_vertex1) * (ratio0 / (ratio0 + ratio1))
+        self._vertex_dict[node.index] = get_rotated_np_vertex(np_vertex0, np_vertex1, radians)
 
 
 if __name__ == '__main__':
