c9dc5afa8a7b0dd3ec3d10653aeeace3e3f9f3cd
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-08-08 19:01:30 +0200

    PartialMesh update

--- core/icosasphere/partial_mesh.py
+++ core/icosasphere/partial_mesh.py
@@ -7,7 +7,8 @@ from pyglobe3d.core.icosasphere.any_mesh import AnyMesh
 class PartialMesh(AnyMesh):
     def __init__(self, partition: int = 1, radius: float = 1.0):
         AnyMesh.__init__(self, partition, radius)
-        self._theta_factor = self.icosahedron.theta / self.logic_mesh.partition
+        self._theta_factor = self.icosahedron.theta / self.logic_mesh.GRID.PARTITION
+        self._triangles_offset = self.logic_mesh.GRID.NUMBER_OF_TRIANGLES
         self._add_icosahedron_vertices()
 
     @property
@@ -17,9 +18,10 @@ class PartialMesh(AnyMesh):
     def add_nodes(self, nodes):
         for node in nodes:
             if node.index not in self._vertex_cash:
-                self._add_edge_nodes(node.nearest_layer_edges, node.nearest_layer_edge_nodes)
-                if node.nearest_layer_edges == 2:  # otherwise the node is on an edge and was already added
-                    self._add_non_edge_node(node)
+                self._add_node(node)
+            for neighbor in node.neighboring_nodes:
+                if neighbor.index not in self._vertex_cash:
+                    self._add_node(neighbor)
 
     def _add_edge_node(self, edge, node):
         vertex0 = self._vertex_cash[edge.icosahedron_nodes.node0.index]
@@ -36,6 +38,13 @@ class PartialMesh(AnyMesh):
         icosahedron_vertices = self.icosahedron.vertex_np_array
         icosahedron_nodes_indices = (node.index for node in self.logic_mesh.ICOSAHEDRON_NODES)
         self._vertex_cash = dict(zip(icosahedron_nodes_indices, icosahedron_vertices))
+
+    def _add_node(self, node):
+        if node.nearest_layer_edges_number == 2:  # The node is not on an edge
+            self._add_edge_nodes(node.nearest_layer_edges, node.nearest_layer_edge_nodes)
+            self._add_non_edge_node(node)
+        else:  # The node is on an edge: node.nearest_layer_edges_number == 1
+            self._add_edge_node(node.nearest_layer_edges.edge0, node)
 
     def _add_non_edge_node(self, node):
         vertex0 = self._vertex_cash[node.nearest_layer_edge_nodes.node0.index]
