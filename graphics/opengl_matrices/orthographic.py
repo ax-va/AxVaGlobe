3ba4bbd6d041bc38e004d12824a1b483229cd90a
Alexander Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-08-09 01:35:04 +0200

    Update orthographic.py

--- graphics/opengl_matrices/orthographic.py
+++ graphics/opengl_matrices/orthographic.py
@@ -24,5 +24,8 @@ class Orthographic(Projection):
             self._matrix[1][3] = -(self._top + self._bottom) / self._top_minus_bottom
         except ZeroDivisionError:
             raise EqualClippingPlanesError('bottom', 'top')
-        self._matrix[2][2] = -2. / self._far_minus_near
-        self._matrix[2][3] = -(self._far + self._near) / self._far_minus_near
+        try:
+            self._matrix[2][2] = -2. / self._far_minus_near
+            self._matrix[2][3] = -(self._far + self._near) / self._far_minus_near
+        except ZeroDivisionError:
+            raise EqualClippingPlanesError('near', 'far')
