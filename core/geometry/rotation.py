bc7c5f147bc12b54ef31d8b552e467a23ce0c33a
Alexander Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-08-08 23:57:20 +0200

    Update rotation.py

--- core/geometry/rotation.py
+++ core/geometry/rotation.py
@@ -4,20 +4,20 @@ import math
 from typing import List
 
 
-def get_angle_between(vertex0: np.array, vertex1: np.array) -> float:
+def get_angle_between(vertex0, vertex1) -> float:
     return math.acos((vertex0[0] * vertex1[0] + vertex0[1] * vertex1[1] + vertex0[2] * vertex1[2]) 
                      / (norm(vertex0) * norm(vertex1)))
 
 
-def norm(vertex: np.array) -> float:
+def norm(vertex) -> float:
     return math.sqrt(vertex[0]**2 + vertex[1]**2 + vertex[2]**2)
 
 
-def normalize(vertex: np.array) -> np.array:
-    return vertex / norm(vertex)
+def normalize_np_vertex(np_vertex: np.array) -> np.array:
+    return np_vertex / norm(np_vertex)
 
 
-def get_rotation_matrix_as_list(x: float, y: float, z: float, cos_t: float, sin_t: float) -> List:
+def get_rotation_list_matrix(x: float, y: float, z: float, cos_t: float, sin_t: float) -> List:
     _1_minus_cos_t = 1. - cos_t
     xy = x * y
     xz = x * z
@@ -27,13 +27,13 @@ def get_rotation_matrix_as_list(x: float, y: float, z: float, cos_t: float, sin_
             [_1_minus_cos_t * xz - sin_t * y, _1_minus_cos_t * yz + sin_t * x, cos_t + _1_minus_cos_t * z**2]]
 
 
-def get_rotated_vertex(vertex0: np.array, vertex1: np.array, radians: float) -> np.array:
-    normal = np.cross(vertex0, vertex1)
-    x, y, z = normalize(normal)
+def get_rotated_np_vertex(np_vertex0: np.array, vertex1: np.array, radians: float) -> np.array:
+    np_normal = np.cross(np_vertex0, np_vertex1)
+    x, y, z = normalize_np_vertex(np_normal)
     cos_t = math.cos(radians)
     sin_t = math.sin(radians)
-    rotation_matrix = np.array(get_rotation_matrix_as_list(x, y, z, cos_t, sin_t))
-    return np.dot(rotation_matrix, vertex0)
+    rotation_np_matrix = np.array(get_rotation_list_matrix(x, y, z, cos_t, sin_t))
+    return np.dot(rotation_np_matrix, vertex0)
 
 
 
