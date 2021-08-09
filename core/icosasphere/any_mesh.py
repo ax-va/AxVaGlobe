e6c1efd8d2d3611385840688a804f14b6c89410d
Alexander Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-08-09 21:02:15 +0200

    Update any_mesh.py

--- core/icosasphere/any_mesh.py
+++ core/icosasphere/any_mesh.py
@@ -6,6 +6,8 @@ class AnyMesh:
     def __init__(self, partition: int = 1, radius: float = 1.0):
         self._logic_mesh = LogicMesh(partition=partition)
         self._icosahedron = Icosahedron(radius=radius)
+        self._partition = partition
+        self._radius = radius
 
     @property
     def logic_mesh(self):
