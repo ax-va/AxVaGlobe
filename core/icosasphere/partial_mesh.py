667ab86186f6f2962608c8ea7d523debaefbe26d
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-08-09 19:24:04 +0200

    Partial mesh update

--- core/icosasphere/partial_mesh.py
+++ core/icosasphere/partial_mesh.py
@@ -1,4 +1,4 @@
-from pyglobe3d.core.geometry.linear_algebra import get_angle_between, get_rotated_vertex, get_midpoint_3, change_radius
+from pyglobe3d.core.geometry.linear_algebra import change_radius, get_angle_between, get_rotated_vertex, get_midpoint_3
 from pyglobe3d.core.icosasphere.any_mesh import AnyMesh
 
 
