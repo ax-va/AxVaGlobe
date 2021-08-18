5361345b25db77bd2ae2dbdda5662ceb5c5c1846
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-08-18 12:36:01 +0200

    Vertex indices

--- core/icosasphere/partial_mesh.py
+++ core/icosasphere/partial_mesh.py
@@ -7,40 +7,40 @@ class PartialMesh(AnyMesh):
     def __init__(self, partition: int = 1, radius: float = 1.0):
         AnyMesh.__init__(self, partition, radius)
         self._add_icosahedron_nodes()
+        self._vertex_indices = set()
 
     @property
-    def vertex_cache(self):
-        return self._vertex_cache
+    def vertices(self):
+        return self._vertices
 
     def add_nodes_with_indices(self, node_indices):
         for node_index in node_indices:
             node = self.logic_mesh.create_node(index=node_index)
-            if node_index not in self._vertex_cache:
+            if node_index not in self._vertices:
                 self._add_node(node)
             for neighbor in node.neighboring_nodes:
-                if neighbor.index not in self._vertex_cache:
+                if neighbor.index not in self._vertices:
                     self._add_node(neighbor)
             for triangle in node.adjacent_triangles:
-                if (triangle.index + self._index_offset) not in self._vertex_cache:
+                if (triangle.index + self._index_offset) not in self._vertices:
                     self._add_triangle_midpoint(triangle)
-            ...
+            self._add_vertex_indices(node)
 
     def _add_edge_node(self, edge, node):
-        node0, node1 = edge.icosahedron_nodes
-        vertex0 = self._vertex_cache[node0.index]
-        vertex1 = self._vertex_cache[node1.index]
-        radians = self._theta_factor * node0.division_ratios.ratio0
-        self._vertex_cache[node.index] = get_rotated_vertex(vertex0, vertex1, radians)
+        vertex0 = self._vertices[edge.icosahedron_nodes.node0.index]
+        vertex1 = self._vertices[edge.icosahedron_nodes.node1.index]
+        radians = self._theta_factor * edge.icosahedron_nodes.division_ratios.ratio0
+        self._vertices[node.index] = get_rotated_vertex(vertex0, vertex1, radians)
 
     def _add_edge_nodes(self, edges, nodes):
         for edge, node in zip(edges, nodes):
-            if node.index not in self._vertex_cache:
+            if node.index not in self._vertices:
                 self._add_edge_node(edge, node)
 
     def _add_icosahedron_nodes(self):
         icosahedron_vertices = self.icosahedron.vertex_array
         icosahedron_nodes_indices = (node.index for node in self.logic_mesh.ICOSAHEDRON_NODES)
-        self._vertex_cache = dict(zip(icosahedron_nodes_indices, icosahedron_vertices))
+        self._vertices = dict(zip(icosahedron_nodes_indices, icosahedron_vertices))
 
     def _add_node(self, node):
         if node.nearest_layer_edges_number == 2:  # The node is not on an edge
@@ -50,23 +50,37 @@ class PartialMesh(AnyMesh):
             self._add_edge_node(node.nearest_layer_edges.edge0, node)
 
     def _add_non_edge_node(self, node):
-        node0, node1 = node.nearest_layer_edge_nodes
-        vertex0 = self._vertex_cache[node0.index]
-        vertex1 = self._vertex_cache[node1.index]
+        vertex0 = self._vertices[node.nearest_layer_edge_nodes.node0.index]
+        vertex1 = self._vertices[node.nearest_layer_edge_nodes.node1.index]
         ratio0, ratio1 = node.division_ratios
         radians = get_angle_between(vertex0, vertex1) * (ratio0 / (ratio0 + ratio1))
-        self._vertex_cache[node.index] = get_rotated_vertex(vertex0, vertex1, radians)
+        self._vertices[node.index] = get_rotated_vertex(vertex0, vertex1, radians)
 
     def _add_triangle_midpoint(self, triangle):
-        node0, node1, node2 = triangle.triangle_nodes
-        vertex0 = self._vertex_cache[node0.index]
-        vertex1 = self._vertex_cache[node1.index]
-        vertex2 = self._vertex_cache[node2.index]
+        vertex0 = self._vertices[triangle.triangle_nodes.node0.index]
+        vertex1 = self._vertices[triangle.triangle_nodes.node1.index]
+        vertex2 = self._vertices[triangle.triangle_nodes.node2.index]
         triangle_midpoint_vertex = get_triangle_midpoint_vertex(vertex0, vertex1, vertex2)
         change_vertex_radius(triangle_midpoint_vertex, self.radius)
-        self._vertex_cache[triangle.index + self._index_offset] = triangle_midpoint_vertex
+        self._vertices[triangle.index + self._index_offset] = triangle_midpoint_vertex
+
+    def _add_vertex_indices(self, node):
+        last = node.adjacent_triangles_number - 1
+        for i in range(last):
+            self._vertex_indices.add(
+                (node.adjacent_triangles[i + 1].index + self._index_offset,
+                 node.adjacent_triangles[i].index + self._index_offset,
+                 node.index)
+            )
+        self._vertex_indices.add(
+            (node.adjacent_triangles[0].index + self._index_offset,
+             node.adjacent_triangles[last].index + self._index_offset,
+             node.index)
+        )
 
 
 if __name__ == '__main__':
     p_msh = PartialMesh(partition=4)
-    print(p_msh.vertex_cache)
+    print(p_msh.vertices)
+    s = set()
+    s.add((1, 2, 3))
