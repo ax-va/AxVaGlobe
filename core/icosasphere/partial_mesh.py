06c998d0ac4ad811b7159493c50c36b2b4e2b236
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-08-22 17:07:36 +0200

    Delete vertices operation

--- core/icosasphere/partial_mesh.py
+++ core/icosasphere/partial_mesh.py
@@ -1,3 +1,4 @@
+from pyglobe3d.warns import PyGlobe3DWarning
 from pyglobe3d.core.geometry.vertex import \
     change_vertex_radius, get_angle_between, get_rotated_vertex, get_triangle_midpoint_vertex
 from pyglobe3d.core.icosasphere.any_mesh import AnyMesh
@@ -7,40 +8,58 @@ class PartialMesh(AnyMesh):
     def __init__(self, partition: int = 1, radius: float = 1.0):
         AnyMesh.__init__(self, partition, radius)
         self._add_icosahedron_nodes()
-        self._vertex_indices = set()
+        self._added_node_indices = set()
+        self._polygon_vertex_indices = set()
 
     @property
-    def vertex_indices(self) -> set:
-        return self._vertex_indices
+    def polygon_vertex_indices(self) -> set:
+        return self._polygon_vertex_indices
 
     @property
     def vertices(self) -> dict:
         return self._vertices
 
     def add_nodes_with_indices(self, node_indices):
+        self._added_node_indices = set.union(self._added_node_indices, set(node_indices))
         for node_index in node_indices:
             node = self.logic_mesh.create_node(index=node_index)
             if node_index not in self._vertices:
                 self._add_node(node)
             self._add_node_neighbors(node)
             self._add_triangle_midpoints(node)
-            self._add_vertex_indices(node)
+            self._change_polygon_vertex_indices(node, set.add)
 
-    def _add_edge_node(self, edge, node):
+    def delete_nodes_with_indices(self, node_indices):
+        self._added_node_indices = set.difference(self._added_node_indices, set(node_indices))
+        for node_index in node_indices:
+            node = self.logic_mesh.create_node(index=node_index)
+            if node_index in self._vertices:
+                if node_index not in self._icosahedron_vertices:
+                    del self._vertices[node_index]
+                    self._delete_node_neighbors(node)
+                    self._delete_triangle_midpoints(node)
+                    self._change_polygon_vertex_indices(node, set.remove)
+                else:
+                    PyGlobe3DWarning(f'the node {node_index} cannot be deleted '
+                                     f'because it is a vertex of the icosahedron')
+            else:
+                PyGlobe3DWarning(f'the node {node_index} was originally not added')
+
+    def _add_edge_node(self, edge, edge_node):
         vertex0 = self._vertices[edge.icosahedron_nodes.node0.index]
         vertex1 = self._vertices[edge.icosahedron_nodes.node1.index]
         radians = self._theta_factor * edge.icosahedron_nodes.division_ratios.ratio0
-        self._vertices[node.index] = get_rotated_vertex(vertex0, vertex1, radians)
+        self._vertices[edge_node.index] = get_rotated_vertex(vertex0, vertex1, radians)
 
-    def _add_edge_nodes(self, edges, nodes):
-        for edge, node in zip(edges, nodes):
-            if node.index not in self._vertices:
-                self._add_edge_node(edge, node)
+    def _add_edge_nodes(self, edges, edge_nodes):
+        for edge, edge_node in zip(edges, edge_nodes):
+            if edge_node.index not in self._vertices:
+                self._add_edge_node(edge, edge_node)
 
     def _add_icosahedron_nodes(self):
-        icosahedron_vertices = self.icosahedron.vertex_array
+        self._icosahedron_vertices = self.icosahedron.vertex_array
         icosahedron_nodes_indices = (node.index for node in self.logic_mesh.ICOSAHEDRON_NODES)
-        self._vertices = dict(zip(icosahedron_nodes_indices, icosahedron_vertices))
+        self._vertices = dict(zip(icosahedron_nodes_indices, self._icosahedron_vertices))
 
     def _add_node(self, node):
         if node.nearest_layer_edges_number == 2:  # The node is not on an edge
@@ -74,19 +93,31 @@ class PartialMesh(AnyMesh):
             if (triangle.index + self._index_offset) not in self._vertices:
                 self._add_triangle_midpoint(triangle)
 
-    def _add_vertex_indices(self, node):
+    def _change_polygon_vertex_indices(self, node, func=set.add):
         last = node.adjacent_triangles_number - 1
         for i in range(last):
-            self._vertex_indices.add(
+            func(self._polygon_vertex_indices,
                 (node.adjacent_triangles[i + 1].index + self._index_offset,
                  node.adjacent_triangles[i].index + self._index_offset,
                  node.index)
             )
-        self._vertex_indices.add(
+        func(self._polygon_vertex_indices,
             (node.adjacent_triangles[0].index + self._index_offset,
              node.adjacent_triangles[last].index + self._index_offset,
              node.index)
         )
+
+    def _delete_node_neighbors(self, node):
+        for neighbor in node.neighboring_nodes:
+            if neighbor.index not in self._added_node_indices:
+                del self._vertices[neighbor.index]
+
+    def _delete_triangle_midpoints(self, node):
+        for triangle in node.adjacent_triangles:
+            if not (triangle.triangle_nodes.node0.index in self._added_node_indices
+                    or triangle.triangle_nodes.node1.index in self._added_node_indices
+                    or triangle.triangle_nodes.node2.index in self._added_node_indices):
+                del self._vertices[triangle.index + self._index_offset]
 
 
 if __name__ == '__main__':
@@ -94,3 +125,10 @@ if __name__ == '__main__':
     print(p_msh.vertices)
     s = set()
     s.add((1, 2, 3))
+    print(s)
+    s.remove((1, 2, 3))
+    print(s)
+    set1 = {2, 4, 5, 6}
+    set2 = {4, 6, 7, 8}
+    s = set.union(set1, set2)
+    print(s)
