d8363ce1577a96a2bf78fa314fba5daf5e9ee49a
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-08-23 15:09:47 +0200

    Library info

--- __init__.py
+++ __init__.py
@@ -3,9 +3,12 @@
 
 __authors__ = ['Alexander Vasiliev']
 __dependencies__ = {
-    'pytest': {'version': '6.2.4'},
+    'pytest': '6.2.4',
+    'pygame': '2.0.1',
+    'PyOpenGL': '3.1.5',
+    'PyOpenGL-accelerate': '3.1.5',
     #  'numpy': {'version': '1.21.1'},
-    #  'numba': {'version': '0.53.1'}
+    #  'numba': {'version': '0.53.1'},
 }
 __library_version__ = '0.0.1'
 __library_release_date__ = '2021-05-15'
