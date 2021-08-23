7f70de2fa1fceef9f970c0a0fc5b01135d78252b
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-08-23 14:56:33 +0200

    Library info

--- __init__.py
+++ __init__.py
@@ -7,6 +7,6 @@ __dependencies__ = {
     #  'numpy': {'version': '1.21.1'},
     #  'numba': {'version': '0.53.1'}
 }
-__library_versions__ = ['0.0.1']
-__library_release_dates__ = ['2021-05-15']
-__python_versions__ = ['3.8']
+__library_version__ = '0.0.1'
+__library_release_date__ = '2021-05-15'
+__python_versions__ = '3.8'
