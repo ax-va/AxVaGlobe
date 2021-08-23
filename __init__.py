0d9ba4e60d51cf0c4ce0ebaf655c2dac00892f2a
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-08-23 13:42:52 +0200

    Library info

--- __init__.py
+++ __init__.py
@@ -1,13 +1,12 @@
 """Logical-graphical library PyGlobe3D"""
 
 
-class PackageInformation:
-    AUTHORS = ['Alexander Vasiliev']
-    DEPENDENCIES = {
-        'pytest': {'version': '6.2.4'},
-        'numpy': {'version': '1.21.1'},
-        'numba': {'version': '0.53.1'}
-    }
-    LIBRARY_VERSIONS = ['0.0.1']
-    LIBRARY_RELEASE_DATES = ['2021-05-15']
-    PYTHON_VERSION = '3.8'
+__authors__ = ['Alexander Vasiliev']
+__dependencies__ = {
+    'pytest': {'version': '6.2.4'},
+    #  'numpy': {'version': '1.21.1'},
+    #  'numba': {'version': '0.53.1'}
+}
+__library_versions__ = ['0.0.1']
+__library_release_dates__ = ['2021-05-15']
+__python_versions__ = ['3.8']
