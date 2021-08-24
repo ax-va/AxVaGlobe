aefb4a533b82af35fbba275663950964fa1e1786
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-08-24 09:18:14 +0200

    Slight update

--- __init__.py
+++ __init__.py
@@ -17,5 +17,5 @@ __dependencies__ = {
     #  'numba': {'version': '0.53.1'},
 }
 __library_version__ = '0.0.1'
-__library_release_date__ = '2021-05-15'
+__library_release_date__ = '2021-xx-xx'
 __python_versions__ = '3.8'
