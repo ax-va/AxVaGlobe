3b39248f7de52c1237f6eee6ddd55a5946adf2dc
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-11-09 13:32:35 +0100

    Restructuring

---
+++ __init__.py
@@ -0,0 +1,23 @@
+__authors__ = ['Alexander Vasiliev']
+__dependencies__ = {
+    'necessary':
+        {
+            'pygame': '2.0.1',
+            'PyOpenGL': '3.1.5',
+            'PyOpenGL-accelerate': '3.1.5',
+        },
+    'not necessary':
+        {
+            'pytest': '6.2.4',
+        },
+    #  'numpy': '1.21.1',
+    #  'numba': '0.53.1',
+}
+__doc__ = """
+    PyGlobe3D is a free open-source logical-graphical Python library 
+    for creating globes by hexagon-like polygons and working with them. 
+    The library is designed for games, education, and simulation.
+    """
+__library_version__ = '0.0.1'
+__library_release_date__ = '2021-xx-xx'
+__python_version__ = '3.8'
