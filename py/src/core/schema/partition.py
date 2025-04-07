156004b15d66d4adb26e981777420714d18e6af5
ax-va <vasiliev.informatik@t-online.de>
2025-04-08 00:35:06 +0200

    update

--- py/src/core/schema/partition.py
+++ py/src/core/schema/partition.py
@@ -1,6 +1,6 @@
 class PartitionValueError(Exception):
     def __init__(self, value: int):
-        self.message = f"The `partition`'s value is not a positive integer: {value}."
+        self.message = f"The partition value is not a positive integer: {value}."
         Exception.__init__(self, self.message)
 
 
