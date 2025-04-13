771ca2256c543482f520ad5270121555c4e83748
ax-va <vasiliev.informatik@t-online.de>
2025-04-13 18:59:35 +0200

    update

--- py/src/core/schema/node_a.py
+++ py/src/core/schema/node_a.py
@@ -14,18 +14,20 @@ class NodeA:
         return self.LAYER_INDEX * 5
 
     def _get_layer_and_in_layer_indices(self) -> Tuple[int, int]:
-        integer_part = self.INDEX // 5
-        remainder = self.INDEX % 5
-        # This is exactly equal to the layer index of the penultimate node in layer
-        to_layer = (sqrt(integer_part * 8 + 1) - 1) / 2
-        num = int(to_layer)
-        sum_up_to_num = ((num + 1) * num) // 2
+        integer_part: int = self.INDEX // 5
+        remainder: int = self.INDEX % 5
+        # This lies between layer_index - 1 and layer_index
+        # and is exactly equal to layer_index of the penultimate node in layer.
+        to_layer: float = (sqrt(integer_part * 8 + 1) - 1) / 2
+        num: int = int(to_layer)
+        sum_up_to_num: int = ((num + 1) * num) // 2
         if integer_part != sum_up_to_num or remainder != 0:
             # non-penultimate node in layer
-            layer_index = num + 1
-            in_layer_index = self.INDEX - sum_up_to_num * 5 - 1
+            layer_index: int = num + 1
+            # sum_up_to_num * 5 is the offset relative to previous layers
+            in_layer_index: int = self.INDEX - sum_up_to_num * 5 - 1
         else:
             # penultimate node in layer
-            layer_index = num
-            in_layer_index = self.INDEX - (sum_up_to_num - num) * 5 - 1
+            layer_index: int = num
+            in_layer_index: int = self.INDEX - (sum_up_to_num - num) * 5 - 1
         return layer_index, in_layer_index
