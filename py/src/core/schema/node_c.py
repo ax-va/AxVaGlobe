9ef1f58d8676eae40bb20e851e90c1d9b67ab7a9
ax-va <vasiliev.informatik@t-online.de>
2025-04-20 00:09:49 +0200

    update

---
+++ py/src/core/schema/node_c.py
@@ -0,0 +1,40 @@
+from math import sqrt
+from typing import Tuple
+
+from core.schema.base_node import BaseNode
+
+
+class NodeC(BaseNode):
+    @classmethod
+    def create_node_by_index(
+            cls,
+            index: int,
+            schema,  # type: "Schema"
+    ):
+        layer_index, index_offset_for_layer = cls._get_layer_index_and_index_offset_for_layer(index, schema)
+        in_layer_index: int = index - index_offset_for_layer
+        return cls(layer_index, in_layer_index, schema)
+
+    @staticmethod
+    def _get_layer_index_and_index_offset_for_layer(
+            index: int,
+            schema,  # type: "Schema"
+    ) -> Tuple[int, int]:
+        end_index: int = schema.constants.south_pole.node.INDEX
+        end_index_for_area_c: int = schema.constants.area_c.nodes.END
+        inverse_index_offset_for_area_c: int = end_index - end_index_for_area_c
+        inverse_index = end_index - index
+        inverse_num: int = (inverse_index - inverse_index_offset_for_area_c) // 5
+        inverse_layer_index_minus_one: int = int((sqrt(inverse_num * 8 + 1) - 1) / 2)
+        inverse_layer_index: int = inverse_layer_index_minus_one + 1
+        end_layer_index: int = schema.constants.south_pole.node_layer.INDEX
+        layer_index: int = end_layer_index - inverse_layer_index
+        inverse_sum_of_previous_layer_indices: int = (inverse_layer_index * inverse_layer_index_minus_one) // 2
+        inverse_index_offset_for_layer: int = inverse_sum_of_previous_layer_indices * 5 + inverse_index_offset_for_area_c
+        layer = schema.get_node_layer(layer_index)  # type: "node layer class"
+        index_offset_for_layer: int = end_index - inverse_index_offset_for_layer - (layer.NUMBER_OF_NODES - 1)
+        return layer_index, index_offset_for_layer
+
+from core.schema.schema import Schema
+node = NodeC(5, 0, Schema(2))
+print(node.INDEX)
---
+++ py/tests/test_core/test_schema/test_node_c.py
@@ -0,0 +1,126 @@
+import pytest
+
+from core.schema.schema import Schema
+from core.schema.node_c import NodeC
+
+
+@pytest.mark.parametrize(
+    "index,"
+    "layer_index,"
+    "in_layer_index",
+    [
+        (36, 5, 0),
+        (37, 5, 1),
+        (38, 5, 2),
+        (39, 5, 3),
+        (40, 5, 4),
+    ]
+)
+def test_creation_of_node_c_for_schema_two(
+    index,
+    layer_index,
+    in_layer_index,
+):
+    schema = Schema(2)
+    node_c = NodeC(layer_index, in_layer_index, schema)
+    assert node_c.INDEX == index
+    assert node_c.LAYER_INDEX == layer_index
+    assert node_c.IN_LAYER_INDEX == in_layer_index
+
+    node_c = NodeC.create_node_by_index(index, schema)
+    assert node_c.INDEX == index
+    assert node_c.LAYER_INDEX == layer_index
+    assert node_c.IN_LAYER_INDEX == in_layer_index
+
+
+@pytest.mark.parametrize(
+    "index,"
+    "layer_index,"
+    "in_layer_index",
+    [
+        (76, 7, 0),
+        (77, 7, 1),
+        (78, 7, 2),
+        (79, 7, 3),
+        (80, 7, 4),
+        (81, 7, 5),
+        (82, 7, 6),
+        (83, 7, 7),
+        (84, 7, 8),
+        (85, 7, 9),
+        (86, 8, 0),
+        (87, 8, 1),
+        (88, 8, 2),
+        (89, 8, 3),
+        (90, 8, 4),
+    ]
+)
+def test_creation_of_node_c_for_schema_three(
+    index,
+    layer_index,
+    in_layer_index,
+):
+    schema = Schema(3)
+    node_c = NodeC(layer_index, in_layer_index, schema)
+    assert node_c.INDEX == index
+    assert node_c.LAYER_INDEX == layer_index
+    assert node_c.IN_LAYER_INDEX == in_layer_index
+
+    node_c = NodeC.create_node_by_index(index, schema)
+    assert node_c.INDEX == index
+    assert node_c.LAYER_INDEX == layer_index
+    assert node_c.IN_LAYER_INDEX == in_layer_index
+
+
+@pytest.mark.parametrize(
+    "index,"
+    "layer_index,"
+    "in_layer_index",
+    [
+        (131, 9, 0),
+        (132, 9, 1),
+        (133, 9, 2),
+        (134, 9, 3),
+        (135, 9, 4),
+        (136, 9, 5),
+        (137, 9, 6),
+        (138, 9, 7),
+        (139, 9, 8),
+        (140, 9, 9),
+        (141, 9, 10),
+        (142, 9, 11),
+        (143, 9, 12),
+        (144, 9, 13),
+        (145, 9, 14),
+        (146, 10, 0),
+        (147, 10, 1),
+        (148, 10, 2),
+        (149, 10, 3),
+        (150, 10, 4),
+        (151, 10, 5),
+        (152, 10, 6),
+        (153, 10, 7),
+        (154, 10, 8),
+        (155, 10, 9),
+        (156, 11, 0),
+        (157, 11, 1),
+        (158, 11, 2),
+        (159, 11, 3),
+        (160, 11, 4),
+    ]
+)
+def test_creation_of_node_c_for_schema_four(
+    index,
+    layer_index,
+    in_layer_index,
+):
+    schema = Schema(4)
+    node_c = NodeC(layer_index, in_layer_index, schema)
+    assert node_c.INDEX == index
+    assert node_c.LAYER_INDEX == layer_index
+    assert node_c.IN_LAYER_INDEX == in_layer_index
+
+    node_c = NodeC.create_node_by_index(index, schema)
+    assert node_c.INDEX == index
+    assert node_c.LAYER_INDEX == layer_index
+    assert node_c.IN_LAYER_INDEX == in_layer_index
