d8636a6180e8535757ca79840756e5917357e2c2
Alexander Vasiliev 🐍 💪 <85578981+ax-va@users.noreply.github.com>
2025-04-21 22:49:26 +0200

    update

--- py/src/core/schema/node_b.py
+++ py/src/core/schema/node_b.py
@@ -28,7 +28,7 @@ class _NodeB(BaseNode):
             Tuple[int, int],
             Tuple[int, int]
     ]:
-
+        """Returns 6 neighboring nodes, each as (layer_index, in_layer_index), all in a tuple."""
         layer_index_0 = self.LAYER_INDEX - 1  # up
         layer_index_1 = layer_index_0  # up
         layer_index_2 = self.LAYER_INDEX
