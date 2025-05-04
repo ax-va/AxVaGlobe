a10ef9b2a2ce0a112856d3d24ae66aeecd59ea35
ax-va <vasiliev.informatik@t-online.de>
2025-05-04 20:36:23 +0200

    update

--- py/src/core/schema/node_a.py
+++ py/src/core/schema/node_a.py
@@ -27,3 +27,75 @@ class _NodeA(BaseNode):
         sum_of_previous_layer_indices: int = (layer_index * layer_index_minus_one) // 2
         index_offset_for_layer: int = sum_of_previous_layer_indices * 5 + index_offset_for_area_a
         return layer_index, index_offset_for_layer
+
+    def get_layer_and_in_layer_indices_of_neighboring_nodes(
+        self,
+    ) -> Tuple[
+            # 6 neighboring nodes indices
+            Tuple[int, int],  # (layer_index, in_layer_index)
+            Tuple[int, int],
+            Tuple[int, int],
+            Tuple[int, int],
+            Tuple[int, int],
+            Tuple[int, int]
+    ]:
+
+        num: int = self.IN_LAYER_INDEX // self.LAYER_INDEX
+        rem: int = self.IN_LAYER_INDEX % self.LAYER_INDEX
+
+        layer_index_0: int = self.LAYER_INDEX - 1  # up
+        layer_index_3: int = self.LAYER_INDEX + 1  # down
+        layer_index_4: int = self.LAYER_INDEX + 1  # down
+        layer_index_5: int = self.LAYER_INDEX
+
+        if rem != 0:  # This node is not on the edge of the icosahedron
+            layer_index_1: int = self.LAYER_INDEX - 1  # up
+            layer_index_2: int = self.LAYER_INDEX
+
+            in_layer_index_4: int = self.IN_LAYER_INDEX + num
+            in_layer_index_5: int = self.IN_LAYER_INDEX - 1
+            in_layer_index_0: int = in_layer_index_5 - num
+            in_layer_index_3: int = in_layer_index_4 + 1
+
+            if self.IN_LAYER_INDEX < self._layer.END_NODE_IN_LAYER_INDEX:
+                in_layer_index_1: int = self.IN_LAYER_INDEX - num
+                in_layer_index_2: int = self.IN_LAYER_INDEX + 1
+
+            else:
+                in_layer_index_1: int = 0
+                in_layer_index_2: int = 0
+
+        else:  # This node is on the edge of the icosahedron
+            layer_index_1: int = self.LAYER_INDEX
+            layer_index_2: int = self.LAYER_INDEX + 1  # down
+
+            if self.IN_LAYER_INDEX != 0:
+                in_layer_index_3: int = self.IN_LAYER_INDEX + num
+                in_layer_index_2: int = in_layer_index_3 + 1
+                in_layer_index_4: int = in_layer_index_3 - 1
+                in_layer_index_0: int = self.IN_LAYER_INDEX - num
+
+                if self.LAYER_INDEX != 1 or self.IN_LAYER_INDEX != 4:
+                    in_layer_index_1: int = self.IN_LAYER_INDEX + 1
+
+                else:
+                    in_layer_index_1: int = 0
+
+                in_layer_index_5: int = self.IN_LAYER_INDEX - 1
+
+            else:
+                in_layer_index_0: int = 0
+                in_layer_index_1: int = 1
+                in_layer_index_2: int = 1
+                in_layer_index_3: int = 0
+                in_layer_index_4: int = (self.LAYER_INDEX - 1) * 5 - 1
+                in_layer_index_5: int = self.LAYER_INDEX * 5 - 1
+
+        return (
+            (layer_index_0, in_layer_index_0),
+            (layer_index_1, in_layer_index_1),
+            (layer_index_2, in_layer_index_2),
+            (layer_index_3, in_layer_index_3),
+            (layer_index_4, in_layer_index_4),
+            (layer_index_5, in_layer_index_5),
+        )
