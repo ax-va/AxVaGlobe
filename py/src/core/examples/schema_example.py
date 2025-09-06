52528ac604fbd578b5fec4fb9e4d9165ff0cf8c8
ax-va <vasiliev.informatik@t-online.de>
2025-09-06 23:00:11 +0200

    examples updated

--- py/src/core/examples/schema_example.py
+++ py/src/core/examples/schema_example.py
@@ -1,23 +1,23 @@
 from core.schema import Schema
 
-schema_four = Schema(4)
+schema_4 = Schema(4)
 
-schema_four_node_25 = schema_four.get_node_by_index(25)
-print(schema_four_node_25)
-# NodeA(3, 9, Constants(4))
-print(schema_four_node_25.INDEX)
+schema_4_node_25 = schema_4.get_node_by_index(25)
+print(schema_4_node_25)
+# _NodeA(3, 9, Constants(4))
+print(schema_4_node_25.INDEX)
 # 25
-print(schema_four_node_25.LAYER_INDEX)
+print(schema_4_node_25.LAYER_INDEX)
 # 3
-print(schema_four_node_25.IN_LAYER_INDEX)
+print(schema_4_node_25.IN_LAYER_INDEX)
 # 9
 
-schema_four_node_100 = schema_four.get_node_by_index(100)
-print(schema_four_node_100)
+schema_4_node_100 = schema_4.get_node_by_index(100)
+print(schema_4_node_100)
 # _NodeB(7, 9, Constants(4))
-print(schema_four_node_100.INDEX)
+print(schema_4_node_100.INDEX)
 # 100
-print(schema_four_node_100.LAYER_INDEX)
+print(schema_4_node_100.LAYER_INDEX)
 # 7
-print(schema_four_node_100.IN_LAYER_INDEX)
+print(schema_4_node_100.IN_LAYER_INDEX)
 # 9
