b3652cd62b344fa97ec122f363e038d1c7cca4d7
ax-va <vasiliev.informatik@t-online.de>
2025-09-07 15:54:50 +0200

    changed to poetry

--- py/src/axvaglobe/core/schema/node_layer_registry.py
+++ py/src/axvaglobe/core/schema/node_layer_registry.py
@@ -16,18 +16,18 @@ class NodeLayerRegistry:
         if constants.PARTITION not in cls._registry:
             cls._registry[constants.PARTITION] = {}
 
-        p_registry: dict[int, NodeLayer] = cls._registry[constants.PARTITION]
-        if node_layer_index not in p_registry:
+        partition_registry: dict[int, NodeLayer] = cls._registry[constants.PARTITION]
+        if node_layer_index not in partition_registry:
             # The node layer instance is not available in the registry.
             # Create a new node layer instance.
             node_layer: NodeLayer = NodeLayerFactory.create_node_layer_by_index(
                 node_layer_index, constants
             )
             # Add the new node layer instance to the registry
-            p_registry[node_layer_index] = node_layer
+            partition_registry[node_layer_index] = node_layer
 
         else:
             # Get the node layer instance from the registry
-            node_layer: NodeLayer = p_registry[node_layer_index]
+            node_layer: NodeLayer = partition_registry[node_layer_index]
 
         return node_layer
