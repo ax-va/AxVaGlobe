2ca1abfe30fd5c829524425532a0106c88c44a6e
ax-va <vasiliev.informatik@t-online.de>
2025-09-29 23:04:39 +0200

    update

--- py/src/axvaglobe/core/examples/schema_example.py
+++ py/src/axvaglobe/core/examples/schema_example.py
@@ -2,22 +2,22 @@ from axvaglobe.core.schema import Schema
 
 schema_4 = Schema(4)
 
-schema_4_node_25 = schema_4.get_node_by_its_index(25)
-print(schema_4_node_25)
+node_25 = schema_4.get_node_by_its_index(25)
+print(node_25)
 # _NodeA(3, 9, Constants(4))
-print(schema_4_node_25.INDEX)
+print(node_25.INDEX)
 # 25
-print(schema_4_node_25.LAYER_INDEX)
+print(node_25.LAYER_INDEX)
 # 3
-print(schema_4_node_25.IN_LAYER_INDEX)
+print(node_25.IN_LAYER_INDEX)
 # 9
 
-schema_4_node_100 = schema_4.get_node_by_its_index(100)
-print(schema_4_node_100)
+node_100 = schema_4.get_node_by_its_index(100)
+print(node_100)
 # _NodeB(7, 9, Constants(4))
-print(schema_4_node_100.INDEX)
+print(node_100.INDEX)
 # 100
-print(schema_4_node_100.LAYER_INDEX)
+print(node_100.LAYER_INDEX)
 # 7
-print(schema_4_node_100.IN_LAYER_INDEX)
+print(node_100.IN_LAYER_INDEX)
 # 9
