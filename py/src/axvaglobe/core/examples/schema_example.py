da92e83730c708fc0e6b7ea597435be1f6357835
ax-va <vasiliev.informatik@t-online.de>
2025-11-17 10:50:13 +0100

    update

--- py/src/axvaglobe/core/examples/schema_example.py
+++ py/src/axvaglobe/core/examples/schema_example.py
@@ -1,10 +1,13 @@
 from axvaglobe.core.schema import Schema
 
-# Get a schema
+# Get a schema by partition
 schema_4 = Schema.get_schema(4)
 
-# Get a node in the schema
+# Get a node by its index in the schema
 node_25 = schema_4.get_node(25)
+
+# Investigate the node
+
 print(node_25)
 # _NodeA(layer_index=3, in_layer_index=9, partition_obj=Partition(partition=4))
 print(node_25.INDEX)
@@ -14,8 +17,11 @@ print(node_25.LAYER_INDEX)
 print(node_25.IN_LAYER_INDEX)
 # 9
 
-# Get a node in the schema
+# Get another node by its index in the schema
 node_100 = schema_4.get_node(100)
+
+# Investigate the node
+
 print(node_100)
 # _NodeB(layer_index=7, in_layer_index=9, partition_obj=Partition(partition=4))
 print(node_100.INDEX)
